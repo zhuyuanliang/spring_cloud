22ac11b2a32856deea202ca52be91143611af321
zhuyuanliang <zhuyuanliang@qgutech.com>
2018-08-13 22:25:24 +0800

    spring_cloud项目注册中心搭建

---
+++ eureka_web/src/main/java/com/spring/cloud/eureka_web/BaseService.java
@@ -0,0 +1,22 @@
+package com.spring.cloud.eureka_web;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@Service
+public class BaseService {
+    @Resource
+    RestTemplate restTemplate;
+
+    @HystrixCommand(fallbackMethod = "getOneFallback")
+    public String getOne() {
+        return restTemplate.getForObject("http://REGISTRY-CLIENT/getOne", String.class);
+    }
+
+    public String getOneFallback() {
+        return "sorry ribbon, it's error!";
+    }
+}
---
+++ eureka_web/src/main/java/com/spring/cloud/eureka_web/IndexController.java
@@ -0,0 +1,18 @@
+package com.spring.cloud.eureka_web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+public class IndexController {
+
+    @Resource
+    private BaseService baseService;
+    @RequestMapping("/hi")
+    public String hi(@RequestParam String id){
+        return baseService.getOne();
+    }
+}
